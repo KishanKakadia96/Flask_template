097c5e29515831fe27f4dcb48c34591d09539377
KishanKakadia96 <kishankakadia96@gmail.com>
2022-08-09 23:54:37 +0200

    Predict function Post method

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template, request, jsonify
 from flask import Response
-import os 
+import os
 
 app = Flask(__name__)
 
-@app.route("/", methods = ["POST", "GET"])
-def index():
+@app.route("/", methods=["POST", "GET"])
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=['POST'])
+def predict():
     try:
         return render_template("index.html")
     except Exception as e:
-        return Response("this is error",e)
-
+        return Response("this is error", e)
 
 if __name__ == '__main__':
     app.run(debug=True)
